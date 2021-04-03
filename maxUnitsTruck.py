49a31d9fad20b7a4a90f1468dd3b33ce60fb8fc4
Stone Yan <40185228+stonecyan@users.noreply.github.com>
2021-04-03 18:18:33 -0400

    Create maxUnitsTruck.py

---
+++ maxUnitsTruck.py
@@ -0,0 +1,13 @@
+#https://leetcode.com/problems/maximum-units-on-a-truck/
+#boxTypes: List[List[int]], truckSize: int
+def maximumUnits(boxTypes, truckSize):
+        boxTypes.sort(key=lambda x:x[1], reverse=True)
+        units=0
+        for b in boxTypes:
+            if truckSize-b[0]>=0:
+                truckSize-=b[0]
+                units+=b[0]*b[1] 
+            else:
+                units+=truckSize*b[1]
+                return units
+        return units
