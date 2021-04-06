01d956f99218448e926be9d4381a2d3bd29364c6
Stone Yan <40185228+stonecyan@users.noreply.github.com>
2021-04-05 22:03:59 -0400

    Create SubstringsLengthKDistinctK.py

---
+++ SubstringsLengthKDistinctK.py
@@ -0,0 +1,2 @@
+#https://www.geeksforgeeks.org/count-number-of-substrings-with-exactly-k-distinct-characters/
+#https://www.geeksforgeeks.org/count-number-of-substrings-with-exactly-k-distinct-characters/
